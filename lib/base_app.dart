9b24e3876bef62aa41448afbc0c74d2b4943812e
Toan NM <gs.hbsky@gmail.com>
2020-07-09 15:54:34 +0700

    Add base_app export.

---
+++ lib/base_app.dart
@@ -0,0 +1,12 @@
+library base_app;
+
+export 'bloc/base_bloc.dart';
+export 'const/colors.dart';
+export 'const/const.dart';
+export 'const/dimens.dart';
+export 'const/string_value.dart';
+export 'package:base_app/popup/show_popup.dart';
+export 'package:base_app/request/base_request.dart';
+export 'package:base_app/widget/base_stateful_widget_state.dart';
+export 'package:base_app/widget/base_widget.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
